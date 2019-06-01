0bc1020d10b3bebdab09adee82450417d5cf2819
Andyfrost22 <43017376+Andyfrost22@users.noreply.github.com>
2019-05-31 18:05:38 -0600

    Update on Login

--- login.js
+++ login.js
@@ -41,7 +41,33 @@ function login(){
 
 }
 
+function newUser(){
+
+  document.getElementById("firstname_para").innerHTML;
+
+//window.alert(user.uid)
+  
+
+  /*var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
+  });*/
+
+}
+
 function logout(){
+  firebase.auth().signOut();
+}
+
+function newUserlogout(){
   firebase.auth().signOut();
 }
 
