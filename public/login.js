4d11932d9036e2f17d347c6ebe9ba277d097d5ea
Rachel Martinez <rachelmartinez101@gmail.com>
2019-07-07 19:34:16 -0600

    Login page fixed - need to fix onClick home error

--- public/login.js
+++ public/login.js
@@ -8,31 +8,32 @@
       appId: "1:533152371568:web:24876187a15d6c91"
     };
       firebase.initializeApp(config); 
+      window.onload = function() {
+        // inititally hide the user welcome
+        document.getElementById("user_div").style.display = "none";
 
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // User is signed in.
+        firebase.auth().onAuthStateChanged(function(user) {
+          if (user) {
+            // User is signed in.
+            document.getElementById("user_div").style.display = "block";
+            document.getElementById("login_form").style.display = "none";
 
-    document.getElementById("user_div").style.display = "block";
-    document.getElementById("login_div").style.display = "none";
+            var user = firebase.auth().currentUser;
 
-    var user = firebase.auth().currentUser;
+            if(user != null){
 
-    if(user != null){
+              var email_id = user.email;
+              document.getElementById("user_para").innerHTML = "User Email : " + email_id;
+            
+            }
+          } else {
+            // No user is signed in.
+            document.getElementById("login_form").style.display = "block";
+            document.getElementById("user_div").style.display = "none";
 
-      var email_id = user.email;
-      document.getElementById("user_para").innerHTML = "User Email : " + email_id;
-    
-    }
-
-  } else {
-    // No user is signed in.
-
-    document.getElementById("user_div").style.display = "none";
-    document.getElementById("login_div").style.display = "block";
-
-  }
-});
+          }
+        });
+      }
 
 function login(){
 
@@ -50,22 +51,23 @@ function login(){
     // ...
   });
 
-var url = "https://us-central1-antibuddies-274a7.cloudfunctions.net/getUserWithEmail";
-var method = "POST";
-var postData = { "email":"ssmith@gmail.com"};
+//START OF POST REQUEST
+// var url = "https://us-central1-antibuddies-274a7.cloudfunctions.net/getUserWithEmail";
+// var method = "POST";
+// var postData = { "email":"ssmith@gmail.com"};
 
 // You REALLY want shouldBeAsync = true.
 // Otherwise, it'll block ALL execution waiting for server response.
-var shouldBeAsync = true;
+// var shouldBeAsync = true;
 
-var request = new XMLHttpRequest();
+//var request = new XMLHttpRequest();
 
 // Before we send anything, we first have to say what we will do when the
 // server responds. This seems backwards (say how we'll respond before we send
 // the request? huh?), but that's how Javascript works.
 // This function attached to the XMLHttpRequest "onload" property specifies how
 // the HTTP response will be handled. 
-request.onload = function () {
+//request.onload = function () {
 
    // Because of javascript's fabulous closure concept, the XMLHttpRequest "request"
    // object declared above is available in this function even though this function
@@ -74,24 +76,24 @@ request.onload = function () {
    // applications.
 
    // You can get all kinds of information about the HTTP response.
-   var status = request.status; // HTTP response status, e.g., 200 for "200 OK"
-   var data = request.responseText; // Returned data, e.g., an HTML document.
-}
+  //  var status = request.status; // HTTP response status, e.g., 200 for "200 OK"
+  //  var data = request.responseText; // Returned data, e.g., an HTML document.
+//}
 
-request.open(method, url, shouldBeAsync);
+// request.open(method, url, shouldBeAsync);
 
-request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+//request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 // Or... request.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
 // Or... whatever
 
 // Actually sends the request to the server.
-request.send(postData);
+// request.send(postData);
 
-}
+// }
 
-function newUser(){
+// function newUser(){
 
-  document.getElementById("firstname_para").innerHTML;
+//   document.getElementById("firstname_para").innerHTML;
 
 //window.alert(user.uid)
   
@@ -106,7 +108,7 @@ function newUser(){
     // ...
   });*/
 
-}
+//}
 
 function home(){
   window.location.href="home.html";
@@ -138,4 +140,5 @@ function newUserlogin(){
     // ...
   });
 
+}
 }
